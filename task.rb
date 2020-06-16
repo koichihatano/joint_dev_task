8e595b936e306f60cf62c0bf39c8aac96c36c198
KOICHI HATANO <hatch.k53@gmail.com>
2020-06-16 10:23:02 +0900

    質問1から5再提出回答 ”

--- task.rb
+++ task.rb
@@ -5,7 +5,7 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-  names.push("斎藤")
+  names.<< ("斎藤")
   print names # ["田中", "佐藤", "佐々木", "高橋", "斎藤"]
 end
 
@@ -14,25 +14,22 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
-  array1.push(array2)
-  array1.flatten!
-  p array1
+ puts array1 + array2
+  
 end
 
 def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
-  p numbers.group_by(&:itself)
-  p numbers.group_by(&:itself).map{ |key, value| [key, value.count] }.to_h
+  p numbers.count(3)
 end
 
 def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-  sports.delete(nil)
-  puts sports # ["サッカー", "フットサル", "野球", "バスケ", "バレー"]
+  sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -42,12 +39,8 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-  if array1.empty?
-    puts true
-  if ! array2.empty?
-    puts false
-  end
-  end
+p array1.empty?
+p array2.empty?
 end
 
 def q6
